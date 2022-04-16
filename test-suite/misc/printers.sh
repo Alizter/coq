611b7822ed1d7d55646a6772900323fc5be1a2f4
Ali Caglayan <alizter@gmail.com>
2022-04-16 18:00:31 +0100

    [misc] fix printers.sh

--- test-suite/misc/printers.sh
+++ test-suite/misc/printers.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-command -v "$coqtop.byte" || { echo "Missing coqtop.byte"; exit 1; }
+CAML_LD_LIBRARY_PATH="$CAML_LD_LIBRARY_PATH:$COQ_STUBLIBS"
+
+command -v "$coqtop_byte" || { echo "Missing coqtop.byte"; exit 1; }
 
 f=$(mktemp)
 {
-    printf 'Drop.\n#directory "../dev";;\n#use "include";;\n#quit;;\n' | "$coqtop.byte" -q
+    printf 'Drop.\n#directory "../../dev";;\n#use "include";;\n#quit;;\n' | "$coqtop_byte" -q
 } 2>&1 | tee "$f"
+
+echo $CAML_LD_LIBRARY_PATH
 
 # if there's an issue in base_include 'go' won't be defined
 # if there's an issue in include_printers it will be an undefined printer
